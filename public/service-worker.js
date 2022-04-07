537e464a8fe3604e44ed1bd758585c27119e4b2d
Era <amirb2467@gmail.com>
2022-04-07 05:45:21 -0700

    fixed file pathing in  service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,8 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
-  "./public/css/styles.css",
-  "./public/js/index.js"
+  "./css/styles.css",
+  "./js/index.js"
 ];
 
 self.addEventListener('fetch', function (e) {
